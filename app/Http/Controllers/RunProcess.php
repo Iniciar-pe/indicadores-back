6f6045f148f94583058e8fce56a9a26289042d29
Gian Carlos <giansolis93@gmail.com>
2022-12-20 20:49:49 -0500

    cambio en proceso de formula

--- app/Http/Controllers/RunProcess.php
+++ app/Http/Controllers/RunProcess.php
@@ -22,20 +22,25 @@ class RunProcess extends Controller
         // Obtenemos la lista de indicadores y criterio
         $indicators = Indicator::select('formula as formulate', 'id_indicador as indicator',
             'formula_mostrar as viewFormulate', 'expresado as expressed', 'nemonico as mnemonic')
-            ->where('estado', 'A')->get();
+            ->where('estado', 'A')
+            ->orderBy('orden', 'asc')
+            ->get();
         $criterion = Criterion::select('id_usuario as user', 'id_empresa as business', 'numero_dias as countDay')
             ->where('id_criterio', $request->get('criterion'))->first();
 
         foreach ($indicators as $key => $value) {
+            $entry = "";
 
             // Validamos si existe cantidad de dias periodo
-            if (strpos($value->formulate, 'VAR_DIAS_PERIODO')) {
+            $separatorVar = "VAR_DIAS_PERIODO";
+            if (preg_match("/{$separatorVar}/i", $value->formulate)) {
                 $value->formulate = str_replace('VAR_DIAS_PERIODO', $criterion->countDay, $value->formulate);
+                $entry .= 'Cantidad dias : ' . $criterion->countDay . ' <br>';
             }
 
             // Validamos si va a usar variables de indicadores y la reemplzamos
-            // if (strpos($value->formulate, 'RES_')) {
-                $response_result = Result::select('nemonico as mnemonic','resultado as result')
+            //if (strpos($value->formulate, 'RES_')) {
+                $response_result = Result::select('nemonico as mnemonic','resultado as result', 'nombre as name')
                 ->join('tbl_indicadores', 'tbl_indicadores.id_indicador', '=', 'tbl_resultados.id_indicador')
                     ->where([
                         'id_criterio' => $request->get('criterion'),
@@ -44,13 +49,15 @@ class RunProcess extends Controller
                     ])
                     ->get();
 
-                    $val = [];
                     foreach ($response_result as $i => $result) {
                         $value->formulate = str_replace('RES_' . $result->mnemonic, $result->result, $value->formulate);
                         $val[$i] = $value->formulate .'-'. $result->mnemonic;
+                        $separatorRes = 'RES_' . $result->mnemonic;
+                        if (preg_match("/{$separatorRes}/i", $value->formulate)) {
+                            $entry .= $result->name . ': ' . number_format($result->result, 2) . ' <br>';
+                        }
                     }
-            // }
-                    $array[$key] = $val;
+            //}
             // obtenemos los valores y reemplzamos en la formula
             $values = Value::select('nemonico as mnemonic', 'valor_pp as previous', 'valor_pa as current', 'descripcion as description')
                 ->where('id_criterio', $request->get('criterion'))
@@ -58,34 +65,33 @@ class RunProcess extends Controller
                 ->get();
 
             // rubros utilizados
-            $entry = "";
-
+            $arr = [];
             foreach($values as $index => $item) {
-
-                if (strpos($value->formulate, 'ANT_' . $item->mnemonic)) {
-                    $entry .= $item->description . ': ' . $item->previous . ' <br>';
-
-                }
-
-                if (strpos($value->formulate, 'ACT_' . $item->mnemonic)) {
-                    $entry .= $item->description . ': ' . $item->current . ' <br>';
+                $ee ="";
+                $separatorAnt = 'ANT_' . $item->mnemonic;
+                if (preg_match("/{$separatorAnt}/i", $value->formulate)) {
+                    $entry .= $item->description . ': ' . number_format($item->previous, 2) . ' <br>';
 
                 }
+                $separatorAct = 'ACT_' . $item->mnemonic;
+                if (preg_match("/{$separatorAct}/i", $value->formulate)) {
+                    $entry .= $item->description . ': ' . number_format($item->current, 2) . ' <br>';
+                    $ee = "entro";
+                }
+
+
                 $value->formulate = str_replace('ANT_' . $item->mnemonic, $item->previous, $value->formulate);
                 $value->formulate = str_replace('ACT_' . $item->mnemonic, $item->current, $value->formulate);
+                $arr[$index] = $value->formulate."-_".$item->mnemonic."_".$entry.":_:".$ee;
 
             }
-
+            $array[$key] = $arr;
             $result = "";
 
             try {
                 $cadena = '$var = '.$value->formulate.';';
                 eval($cadena);
                 $result = $var;
-
-                if($value->expressed == '2') {
-                    $result = $result * 100;
-                }
             } catch (\Throwable $th) {
                 $result = null;
             }
